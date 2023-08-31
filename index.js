af00ad3402561b700a003a8b769220d336d65aeb
DanPark13 <dpark0703@gmail.com>
2023-08-31 09:09:24 -0400

    Adding & Removing Books Capabilities

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ function Book(title, author, pages){
     }
 }
 
-// Adds Books to Global Library
-function addBooktoLibrary(book){
-    myLibrary.push(book);
-}
-
 // Prints all the books in the library
 function allBooks(library){
     library.forEach(book => {
@@ -28,3 +23,39 @@ theHobbit.info()
 addBooktoLibrary(theHobbit)
 
 allBooks(myLibrary)
+
+document.getElementById('showDialogButton').addEventListener('click', () => {
+    document.getElementById('bookDialog').showModal();
+});
+
+document.getElementById('bookForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+
+    const newBook = new Book(title, author, pages)
+
+    addBooktoLibrary(newBook);
+    
+    document.getElementById('bookDialog').close();
+});
+
+function addBooktoLibrary(book) {
+    const tableBody = document.getElementById('bookTableBody');
+
+    const newRow = tableBody.insertRow();
+
+    newRow.insertCell(0).innerText = book.title;
+    newRow.insertCell(1).innerText = book.author;
+    newRow.insertCell(2).innerText = book.pages;
+    
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.onclick = function() {
+        tableBody.removeChild(newRow);
+    };
+
+    newRow.insertCell(3).appendChild(deleteButton);
+}
